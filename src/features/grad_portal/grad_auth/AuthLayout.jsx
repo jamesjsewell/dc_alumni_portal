852dac591d537b4820511e000d65b9549ced5b31
james sewell <jamesjosephsewell@gmail.com>
2018-07-11 21:14:48 -0500

    added forgot password modal, still have to implement functionality

--- src/features/grad_portal/grad_auth/AuthLayout.jsx
+++ src/features/grad_portal/grad_auth/AuthLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import LoginForm from "./LoginForm.jsx"
 import RegisterForm from "./RegisterForm.jsx"
+import ForgotPasswordForm from "./ForgotPasswordForm.jsx"
 
 //material-ui
 import Grid from '@material-ui/core/Grid'
@@ -12,12 +13,36 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+
 export const AuthLayout = (props) => {
 
-    return(<div>
-    <Grid container spacing={24}>
-        <LoginForm login={props.login} />
-        <RegisterForm register={props.register} />
-    </Grid></div>)
+    return(
+    <div>
+        <Grid container spacing={24}>
+            <LoginForm login={props.login} />
+            <RegisterForm register={props.register} />
+        </Grid>
+        <Button>forgot password?</Button>
+        <Dialog
+            fullScreen={false}
+            open={true}
+            // onClose={this.handleClose}
+            aria-labelledby="responsive-dialog-title"
+        >
+            <DialogTitle id="responsive-dialog-title">{"Change Password"}</DialogTitle>
+            <DialogContent>
+            <DialogContentText>
+                enter the new password you wish to use
+            </DialogContentText>
+                <ForgotPasswordForm />
+            </DialogContent>
+        </Dialog>
+    </div>)
 
 }
---
+++ src/features/grad_portal/grad_auth/ForgotPasswordForm.jsx
@@ -0,0 +1,104 @@
+{/* <Form onSubmit={handleSubmit((formProps)=>{requestPasswordAction(formProps)})}>
+
+    {sendSuccessful
+        ? <Segment>check your email and follow the link</Segment>
+        : <Field
+                name="email"
+                component={FormField}
+                type="text"
+                label="enter your email"
+                placeholder="enter email"
+                required={false}
+            />}
+
+    {sendSuccessful
+        ? <div />
+        : <Field
+                name="emailConfirm"
+                component={FormField}
+                type="text"
+                placeholder="confirm email"
+                required={false}
+            />}
+
+    <Message
+        visible={this.state.messageShowing ? true : false}
+        hidden={this.state.messageShowing ? false : true}
+        floating
+        content={stateOfSend}
+    />
+
+    <Button type="submit" loading={sendingEmail}>
+        {sendSuccessful ? "resend email" : "send email"}
+    </Button>
+
+</Form> */}
+import React, { Component } from "react";
+import { Form, Field, reduxForm, change, reset } from "redux-form";
+//import { alphaNumeric, required, shouldAsyncValidate, asyncValidate } from "../../util/forms/formValidation.js"
+
+import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { FormField } from "./FormFields.jsx"
+
+
+const afterSubmit = (result, dispatch, props) => {
+    props.reset();
+    props.untouch(["email", "password"]);
+
+}
+
+class ForgotPasswordForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+
+    }
+
+    componentWillReceiveProps(nextProps){
+
+        //to reset the form
+        //this.props.reset()
+        
+    }
+
+    doThisOnSubmit(input) {
+       
+        this.props.register({email: input.email, password: input.password, fname: "testting", lname: "testinng"})
+       
+    }
+
+    render() {
+
+        const { handleSubmit } = this.props
+
+        return (
+    
+            <form onSubmit={handleSubmit(this.doThisOnSubmit.bind(this))}>
+
+                <Field type="email" name="email" label="email" component={FormField} />
+                <Field type="email" name="email_confirm" label="confirm email" component={FormField} />
+
+                <Button type="submit">
+                    send email
+                </Button>
+
+            </form > 
+            
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'forgot_password',
+    // fields: ["name"],
+    // asyncValidate: (values, dispatch, validationType)=>{ return asyncValidate(values, dispatch, validationType, 'itemForm') },
+    // asyncBlurFields: ["name"],
+    // shouldAsyncValidate,
+    onSubmitSuccess: afterSubmit
+})(ForgotPasswordForm);
