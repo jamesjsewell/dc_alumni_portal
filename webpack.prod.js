6c1824270e4f946d156967e5a065cd09da5cdbdc
james sewell <jamesjosephsewell@gmail.com>
2018-08-15 15:54:34 -0500

    updated webpack to make a build optimized for surgesh

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.common.js')
 const webpack = require('webpack') // to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const postcssPresetEnv = require('postcss-preset-env')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -38,10 +39,16 @@ module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new UglifyJSPlugin({sourceMap: true}),
-    new ExtractTextPlugin('style.css')
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      title: 'webpack_output',
+      template: './src/index.html',
+      filename: '200.html'
+    })
   ],
   output: {
     filename: 'webpack.bundle.js',
+    publicPath: '/',
     path: path.resolve(__dirname, 'dist')
   }
 
